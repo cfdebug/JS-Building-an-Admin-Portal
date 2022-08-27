7fc4e82d100ccad22f66e31c56299acc4b073f21
cfdebug <rfadventure22@gmail.com>
2022-08-27 00:49:17 -0500

    Completed Bonus

--- public/admin.js
+++ public/admin.js
@@ -1,6 +1,6 @@
-
 // Your Code Here
-async function main() {
+
+async function mainAdmin() {
 
     let response = await fetch('http://127.0.0.1:3001/listBooks')
     let books = await response.json()
@@ -28,14 +28,14 @@ function createList(book){
     input.classList.add('form-control');
     saveBtn.textContent = "Save";
     saveBtn.id = 'basic-addon2';
-    saveBtn.addEventListener("click",function(quantity){
+    saveBtn.addEventListener("click",function(){
         let options = { method: 'PATCH', headers: {'Content-Type':'application/json'},body: JSON.stringify({'id':book.id, 'quantity':input.value})}
         fetch('http://127.0.0.1:3001/updateBook',options);
     });
     deleteBtn.textContent = "Delete";
     deleteBtn.id = 'basic-addon2';
     deleteBtn.style.background = 'red';
-    deleteBtn.addEventListener("click",function(quantity){
+    deleteBtn.addEventListener("click",function(){
         let options = { method: 'DELETE', headers: {'Content-Type':'application/json'},body: JSON.stringify({'id':book.id})}
         fetch(`http://127.0.0.1:3001/removeBook/${book.id}`,options);
     });
@@ -46,4 +46,59 @@ function createList(book){
     element.appendChild(deleteBtn);
 }
 
-main()
+
+function addForm(){
+    let header = document.createElement('h1');
+    let title = document.createElement('input');
+    let titleLbl = document.createElement('label');
+    let desc = document.createElement('input');
+    let descLbl = document.createElement('label');
+    let img = document.createElement('input');
+    let imgLbl = document.createElement('label');
+    let addBook = document.createElement('button');
+
+    header.textContent = 'Add a New Book';
+
+    title.id = 'book-title';
+    title.classList.add('form-control');
+    title.style.marginBottom = '20px';
+
+    titleLbl.setAttribute('for','#book-title')
+    titleLbl.classList.add('form-label');
+    titleLbl.textContent = "Book Title";
+
+    desc.id = 'book-desc';
+    desc.classList.add('form-control');
+    desc.style.marginBottom = '20px';
+
+    descLbl.setAttribute('for','#book-desc')
+    descLbl.classList.add('form-label');
+    descLbl.textContent = "Book Description";
+
+    img.id = 'book-image';
+    img.classList.add('form-control');
+
+    imgLbl.setAttribute('for','#book-image')
+    imgLbl.classList.add('form-label');
+    imgLbl.textContent = "Book Image FileName";
+
+    addBook.textContent = "Add Book";
+    addBook.id = 'basic-addon2';
+    addBook.addEventListener("click",function(){
+        let options = { method: 'POST', headers: {'Content-Type':'application/json'},body: JSON.stringify({'title': title.value, 'year': new Date().getFullYear(), 'description': desc.value, 'quantity': 1, 'imageURL': `assets/${img.value}`})}
+        fetch('http://127.0.0.1:3001/addBook',options);
+        main();
+    });
+
+    document.body.append(header);
+    document.body.append(titleLbl);
+    document.body.append(title);
+    document.body.append(descLbl);
+    document.body.append(desc);
+    document.body.append(imgLbl);
+    document.body.append(img);
+    document.body.append(addBook);
+}
+
+mainAdmin()
+addForm()
